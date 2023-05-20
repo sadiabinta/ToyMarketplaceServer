7d4df719db00710f88e36c40e97638d9f0f8ca3e
Sadia Binta Kabir <sadiaa.kabir@gmail.com>
2023-05-20 12:02:12 -0400

    delete operation done

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app=express();
 const port = process.env.PORT || 5000;
@@ -40,16 +40,27 @@ async function run() {
       res.send(result);
     })
 
-    app.get('/toys',async(req,res)=>{
+    app.get('/allToys',async(req,res)=>{
       let query={};
       if(req.query?.email){
         query={email:req.query.email}
       }
       const result =await toyCollection.find(query).toArray();
+      res.send(result)
+    })
+
+    app.get('/allToys/:id',async (req,res)=>{
+      const id=req.params.id;
+      const query={_id:new ObjectId(id)}
+      const result=await toyCollection.findOne(query);
       res.send(result);
+    })
 
-
-
+    app.delete('/allToys/:id',async(req,res)=>{
+      const id=req.params.id;
+      const query={_id:new ObjectId(id)}
+      const result=await toyCollection.deleteOne(query);
+      res.send(result);
     })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
