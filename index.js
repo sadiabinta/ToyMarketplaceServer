63843617da1aa7488851734764d8e1ab202fb451
Sadia Binta Kabir <sadiaa.kabir@gmail.com>
2023-05-20 07:24:25 -0400

    data load with email query done

--- index.js
+++ index.js
@@ -39,6 +39,18 @@ async function run() {
       const result=await cursor.toArray();
       res.send(result);
     })
+
+    app.get('/toys',async(req,res)=>{
+      let query={};
+      if(req.query?.email){
+        query={email:req.query.email}
+      }
+      const result =await toyCollection.find(query).toArray();
+      res.send(result);
+
+
+
+    })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
