169a04496f30ce25bfe587b158d6d691b4e19ae8
Sadia Binta Kabir <sadiaa.kabir@gmail.com>
2023-05-21 04:22:20 -0400

    finally worked

--- index.js
+++ index.js
@@ -6,7 +6,14 @@ const app=express();
 const port = process.env.PORT || 5000;
 
 // middleware
-app.use(cors());
+
+const corsConfig={
+  origin:'',
+  credentials:true,
+  methods:['GET','POST','PUT','DELETE']
+}
+app.use(cors(corsConfig))
+app.options('',cors(corsConfig))
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.MPDB_USER}:${process.env.MPDB_PASS}@cluster0.cnar9k1.mongodb.net/?retryWrites=true&w=majority`;
@@ -27,9 +34,33 @@ async function run() {
 
     const toyCollection=client.db('toyMarketplace').collection('toyCollection');
 
-    app.get('/allToys', async(req,res)=>{
-      const cursor=toyCollection.find();
-      const result=await cursor.toArray();
+    app.get("/allToys/:text", async(req,res)=>{
+      console.log(req.params.text);
+      if(req.params.text == 'car' || req.params.text == 'actionFigure' || req.params.text == 'doll'){
+        const result= await toyCollection.find({subCategory:req.params.text}).toArray();
+        return res.send(result);
+      }
+      app.get('/allToys/:text',async (req,res)=>{
+        const tt=req.params.text;
+        const query={subCategory:tt}
+        const result=await toyCollection.findOne(query);
+        res.send(result);
+      })
+      // const cursor=toyCollection.find();
+      // const result=await cursor.toArray();
+      // res.send(result);
+    })
+
+    app.get('/myToys/:email',async(req,res)=>{
+      console.log(req.params.email);
+      const result =await toyCollection.find({email:req.params.email}).toArray();
+      res.send(result);
+    })
+
+    app.get('/allToys/:id',async (req,res)=>{
+      const id=req.params.id;
+      const query={_id:new ObjectId(id)}
+      const result=await toyCollection.findOne(query);
       res.send(result);
     })
 
@@ -42,21 +73,7 @@ async function run() {
     });
 
     
-    app.get('/allToys',async(req,res)=>{
-      let query={};
-      if(req.query?.email){
-        query={email:req.query.email}
-      }
-      const result =await toyCollection.find(query).toArray();
-      res.send(result)
-    })
-
-    app.get('/allToys/:id',async (req,res)=>{
-      const id=req.params.id;
-      const query={_id:new ObjectId(id)}
-      const result=await toyCollection.findOne(query);
-      res.send(result);
-    })
+    
 
     app.delete('/allToys/:id',async(req,res)=>{
       const id=req.params.id;
